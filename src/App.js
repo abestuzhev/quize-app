ebbfcb72353adc759950c95277a80cc5d1384614
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2020-04-04 16:29:38 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Car from './Car/Car.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
+    constructor(props){
+        console.log('App constructor');
+        super(props);
+        this.state = {
+            cars: [
+                // {name:'Ford', year: 2020},
+                // {name:'Audi', year: 2017},
+                {name:'Mazda', year: 2016}
+            ],
+            pageTitle: 'React components',
+            showCars: false
+        }
+    }
+
+    
+
+    toggleCarsHandler = () => {
+        this.setState({
+            showCars: !this.state.showCars
+        });
+    }
+
+    onChangeName(name, index){
+        const car = this.state.cars[index]
+        car.name = name;
+        // const cars = this.state.cars.concat(); //clone array
+        const cars = [...this.state.cars]; //clone array ES6
+        cars[index] = car;
+        this.setState({
+            cars: cars
+        });
+    }
+
+    deleteHandler(index) {
+        const cars = this.state.cars.concat();
+        cars.splice(index, 1);
+        this.setState({
+            cars: cars
+        });
+    }   
+    
+    componentWillMount(){
+        console.log('App componentWillMount');
+    }
+
+    componentDidMount(){
+        console.log('App componentDidMount');
+    }
+    
+    render() { 
+        console.log('App Render');
+        const divStyle = {
+            textAlign: 'center'
+        };
+
+        let cars = null;
+        if(this.state.showCars){
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car 
+                        key= {index}
+                        name={car.name} 
+                        year={car.year}
+                        onDelete = {this.deleteHandler.bind(this, index)}
+                        onChangeName = { (event) => this.onChangeName(event.target.value, index) }
+                    />
+                )
+            })
+        }
+
+        
+        return ( 
+            <div style={divStyle}>
+                <h2> {this.state.pageTitle} </h2> 
+
+                <div className="c-row">
+                <button 
+                onClick={this.toggleCarsHandler} className="c-btn">
+                    Toggle cars</button>
+                </div>
+                
+
+                
+    
+                <div className="card-list" style={{width:400, margin: 'auto'}}>
+                    {
+                    cars
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+ 
 export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Car.css';
+
+class Car extends React.Component {
+
+    // жизненные циклы изменения компонента
+    // componentWillReceiveProps(nextProps){
+    //     console.log('Car componentWillReceiveProps', nextProps );
+    // }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.log('Car shouldComponentUpdate', nextProps, nextState );
+        return nextProps.name.trim() !== this.props.name.trim();
+    }
+
+    // componentWillUpdate(nextProps, nextState){
+    //     console.log('Car componentWillUpdate', nextProps, nextState );
+    // }
+
+    static getDerivedStateFromProps(nextProps, prevState){
+        console.log('Car getDirivedStateFromProps', nextProps, prevState ); 
+        return prevState;
+    }
+
+    componentDidUpdate(){
+        console.log('Car componentDidUpdate');
+    }
+
+    getSnapshotBeforeUpdate(){
+        console.log('Car getSnapshotBeforeUpdate');
+    }
+
+    componentWillUnmount(){
+        console.log('Car componentWillUnmount');
+    }
+
+    render(){
+        console.log('Car render');
+        const inputClasses = ['c-input'];
+
+        if(this.props.name !== ''){
+            inputClasses.push('green');
+        }else {
+            inputClasses.push('red');
+        }
+    
+        if(this.props.name.length > 4) {
+            inputClasses.push('bold');
+        }
+    
+        return (
+            <div className="card-list__item">
+                <div className="car-card" >
+                    <h3>Car name: {this.props.name}</h3>
+                    <strong>Year: {this.props.year}</strong>
+                    <div>
+                        <input 
+                        type="text" 
+                        className={inputClasses.join(' ')} 
+                        onChange = {this.props.onChangeName} 
+                        value={this.props.name}/>
+                    </div>
+                    
+                    <button onClick={this.props.onDelete} className="c-btn-del"></button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Car;
