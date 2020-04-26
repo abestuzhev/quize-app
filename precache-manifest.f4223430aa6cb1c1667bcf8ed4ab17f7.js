62eef9546ee20f45c9d4bf5f214f0206b30f1f26
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2020-04-26 19:55:01 +0300

    Updates

---
+++ precache-manifest.f4223430aa6cb1c1667bcf8ed4ab17f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83707fbe587e34c21298bbe6b3ccb7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748d4d1da2cd11cbeb40",
+    "url": "/static/css/main.cabe4e82.chunk.css"
+  },
+  {
+    "revision": "33d7b98c2294151bfb25",
+    "url": "/static/js/2.964c11d4.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/static/js/2.964c11d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748d4d1da2cd11cbeb40",
+    "url": "/static/js/main.0efde5b7.chunk.js"
+  },
+  {
+    "revision": "93e900a4b291d67fac51",
+    "url": "/static/js/runtime-main.97ff3efb.js"
+  }
+]);
--- src/store/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import quizeReducer from './quiz';
-
-export default combineReducers({
-    quize: quizeReducer
-})
